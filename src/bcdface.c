3a3fd1de4b0153117d0459c58777525f9faa3c9f
Mark Shroyer <code@markshroyer.com>
2013-12-30 02:35:38 -0500

    Parameterize, center the columns

--- src/bcdface.c
+++ src/bcdface.c
@@ -3,15 +3,28 @@
 #define SHOW_SECONDS 1
 #define RADIUS 8
 
+#ifdef SHOW_SECONDS
+#define NUM_COLUMNS 6
+#else
+#define NUM_COLUMNS 4
+#endif
+
 static Window *window;
 static Layer *main_layer;
 
+/* offset of the first column */
+int16_t col_offset;
+
+/* spacing between adjacent columns */
+int16_t col_spacing;
+
 static void draw_digit(Layer *layer, GContext *ctx,
-		       int16_t x_coord, int bits, int val)
+                       int col, int bits, int val)
 {
 	GRect bounds = layer_get_bounds(layer);
 	GPoint point;
 	int i;
+	int16_t x_coord = col_offset + RADIUS + (2 * RADIUS + col_spacing) * col;
 
 	int mask = 1;
 	for (i = 0; i < bits; i++) {
@@ -33,12 +46,14 @@ static void update_proc(Layer *layer, GContext *ctx)
 	graphics_context_set_stroke_color(ctx, GColorWhite);
 	graphics_context_set_fill_color(ctx, GColorWhite);
 
-	draw_digit(layer, ctx, RADIUS,      2, now->tm_hour / 10);
-	draw_digit(layer, ctx, 4 * RADIUS,  4, now->tm_hour % 10);
-	draw_digit(layer, ctx, 7 * RADIUS,  3, now->tm_min  / 10);
-	draw_digit(layer, ctx, 10 * RADIUS, 4, now->tm_min  % 10);
-	draw_digit(layer, ctx, 13 * RADIUS, 3, now->tm_sec  / 10);
-	draw_digit(layer, ctx, 16 * RADIUS, 4, now->tm_sec  % 10);
+	draw_digit(layer, ctx, 0, 2, now->tm_hour / 10);
+	draw_digit(layer, ctx, 1, 4, now->tm_hour % 10);
+	draw_digit(layer, ctx, 2, 3, now->tm_min  / 10);
+	draw_digit(layer, ctx, 3, 4, now->tm_min  % 10);
+#ifdef SHOW_SECONDS
+	draw_digit(layer, ctx, 4, 3, now->tm_sec  / 10);
+	draw_digit(layer, ctx, 5, 4, now->tm_sec  % 10);
+#endif
 }
 
 void handle_tick(struct tm *tick_time, TimeUnits units_changed)
@@ -54,9 +69,20 @@ static void window_load(Window *window) {
 		.origin = { 0, 0 },
 		.size = { bounds.size.w, bounds.size.h }
 	});
+#ifdef SHOW_SECONDS
 	tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
+#else
+	tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
+#endif
 	layer_set_update_proc(main_layer, update_proc);
 	layer_add_child(window_layer, main_layer);
+
+	col_spacing = (bounds.size.w - 2 * NUM_COLUMNS * RADIUS) / (NUM_COLUMNS + 1);
+	col_offset = (bounds.size.w - col_spacing * (NUM_COLUMNS - 1) - 2 * NUM_COLUMNS * RADIUS) / 2;
+
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "bounds.size.w = %d", bounds.size.w);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "col_spacing = %d", col_spacing);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "col_offset = %d", col_offset);
 }
 
 static void window_unload(Window *window) {
