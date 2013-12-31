aa31dbeef9d6ee47c2cf284d6f8149e02b956188
Mark Shroyer <code@markshroyer.com>
2013-12-31 02:36:24 -0500

    Separate HMSD layers
    Creates separate layers for hours, minutes, seconds, and the date, on the
    theory that these can be marked dirty and redrawn individually to minimize
    computational overhead.  In reality it doesn't work out quite this way, as
    marking any layer within a window dirty forces a redraw of all layers.

--- src/bcdface.c
+++ src/bcdface.c
@@ -12,9 +12,15 @@
 
 #define DATE_STR_SZ 11
 
+
 static Window *window = NULL;
-static Layer *main_layer = NULL;
-static TextLayer *date_layer = NULL;
+static Layer *layer_hour = NULL;
+static Layer *layer_min = NULL;
+#ifdef SHOW_SECONDS
+static Layer *layer_sec = NULL;
+#endif
+static TextLayer *layer_date = NULL;
+static struct tm *now = NULL;
 static char *date_str = NULL;
 
 /* offset of the first column */
@@ -23,11 +29,23 @@ static int16_t col_offset;
 /* spacing between adjacent columns */
 static int16_t col_spacing;
 
+
+/*
+ * Drawing and tick handler functions
+ *
+ * Handles separate layers for seconds, minutes, hours, and the date string
+ * on the theory that it is significantly less computationally & battery
+ * intensive to figure out which of these layers needs to be updated and
+ * invalidate them individually, than to redraw it all together once per
+ * second.  I haven't emperically determined that this is actually more
+ * energy efficient than the naive implementation though...
+ */
+
 static void draw_digit(Layer *layer, GContext *ctx,
                        int col, int bits, int val)
 {
 	const GRect bounds = layer_get_bounds(layer);
-	const int16_t x_coord = col_offset + RADIUS + (2 * RADIUS + col_spacing) * col;
+	const int16_t x_coord = RADIUS + (2 * RADIUS + col_spacing) * col;
 	GPoint point;
 	int i;
 	int mask = 1;
@@ -43,61 +61,125 @@ static void draw_digit(Layer *layer, GContext *ctx,
 	}
 }
 
-static void update_proc(Layer *layer, GContext *ctx)
+static inline void prepare_ctx(GContext *ctx)
 {
-	const time_t now_time = time(NULL);
-	const struct tm *now = localtime(&now_time);
-
 	graphics_context_set_stroke_color(ctx, GColorWhite);
 	graphics_context_set_fill_color(ctx, GColorWhite);
+}
 
-	strftime(date_str, DATE_STR_SZ, "%a %b %d", now);
-	text_layer_set_text(date_layer, date_str);
+static void update_hour(Layer *layer, GContext *ctx)
+{
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "update_hour()");
 
+	prepare_ctx(ctx);
 	draw_digit(layer, ctx, 0, 2, now->tm_hour / 10);
 	draw_digit(layer, ctx, 1, 4, now->tm_hour % 10);
-	draw_digit(layer, ctx, 2, 3, now->tm_min  / 10);
-	draw_digit(layer, ctx, 3, 4, now->tm_min  % 10);
-#ifdef SHOW_SECONDS
-	draw_digit(layer, ctx, 4, 3, now->tm_sec  / 10);
-	draw_digit(layer, ctx, 5, 4, now->tm_sec  % 10);
-#endif
 }
 
-void handle_tick(struct tm *tick_time, TimeUnits units_changed)
+static void update_min(Layer *layer, GContext *ctx)
 {
-	layer_mark_dirty(main_layer);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "update_min()");
+
+	prepare_ctx(ctx);
+	draw_digit(layer, ctx, 0, 3, now->tm_min / 10);
+	draw_digit(layer, ctx, 1, 4, now->tm_min % 10);
 }
+
+#ifdef SHOW_SECONDS
+
+static void update_sec(Layer *layer, GContext *ctx)
+{
+	prepare_ctx(ctx);
+	draw_digit(layer, ctx, 0, 3, now->tm_sec / 10);
+	draw_digit(layer, ctx, 1, 4, now->tm_sec % 10);
+}
+
+#endif /* defined SHOW_SECONDS */
+
+static void timer_tick(struct tm *tick_time, TimeUnits units_changed)
+{
+	now = tick_time; /* TODO is this safe? */
+
+#ifdef SHOW_SECONDS
+	layer_mark_dirty(layer_sec);
+	if (tick_time->tm_sec != 0)
+		return;
+#endif
+
+	layer_mark_dirty(layer_min);
+	if (tick_time->tm_min != 0)
+		return;
+
+	layer_mark_dirty(layer_hour);
+	if (tick_time->tm_hour != 0)
+		return;
+
+	strftime(date_str, DATE_STR_SZ, "%a %b %d", now);
+	text_layer_set_text(layer_date, date_str);
+}
+
+
+/*
+ * Setup / teardown stuff
+ */
 
 static void window_load(Window *window) {
-        Layer *window_layer = window_get_root_layer(window);
-        GRect bounds = layer_get_bounds(window_layer);
-
-	date_layer = text_layer_create((GRect) {
-		.origin = { 0, 0 },
-		.size = { bounds.size.w, 40 }
-	});
-	main_layer = layer_create((GRect) {
-		.origin = { 0, 0 },
-		.size = { bounds.size.w, bounds.size.h }
-	});
-#ifdef SHOW_SECONDS
-	tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
-#else
-	tick_timer_service_subscribe(MINUTE_UNIT, handle_tick);
-#endif
-	layer_set_update_proc(main_layer, update_proc);
-	layer_add_child(window_layer, main_layer);
-	layer_add_child(window_layer, text_layer_get_layer(date_layer));
-
-	text_layer_set_text(date_layer, "");
-	text_layer_set_background_color(date_layer, GColorBlack);
-	text_layer_set_text_color(date_layer, GColorWhite);
-	text_layer_set_text_alignment(date_layer, GTextAlignmentCenter);
-	text_layer_set_font(date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+        Layer *layer_window = window_get_root_layer(window);
+        GRect bounds = layer_get_bounds(layer_window);
+	const time_t now_time = time(NULL);
 
 	col_spacing = (bounds.size.w - 2 * NUM_COLUMNS * RADIUS) / (NUM_COLUMNS + 1);
 	col_offset = (bounds.size.w - col_spacing * (NUM_COLUMNS - 1) - 2 * NUM_COLUMNS * RADIUS) / 2;
+
+	const int16_t unit_width = 4 * RADIUS + col_spacing;
+	const int16_t unit_offset = unit_width + col_spacing;
+
+	/* This will be needed the first time the update procs run */
+	now = localtime(&now_time);
+
+	layer_hour = layer_create((GRect) {
+		.origin = { col_offset, 0 },
+		.size = { unit_width, bounds.size.h }
+	});
+	layer_set_update_proc(layer_hour, update_hour);
+
+	layer_min = layer_create((GRect) {
+		.origin = { col_offset + unit_offset, 0 },
+		.size = { unit_width, bounds.size.h }
+	});
+	layer_set_update_proc(layer_min, update_min);
+
+#ifdef SHOW_SECONDS
+	layer_sec = layer_create((GRect) {
+		.origin = { col_offset + 2 * unit_offset, 0 },
+		.size = { unit_width, bounds.size.h }
+	});
+	layer_set_update_proc(layer_sec, update_sec);
+#endif
+
+	layer_date = text_layer_create((GRect) {
+		.origin = { 0, 0 },
+		.size = { bounds.size.w, 40 }
+	});
+
+	layer_add_child(layer_window, layer_hour);
+	layer_add_child(layer_window, layer_min);
+#ifdef SHOW_SECONDS
+	layer_add_child(layer_window, layer_sec);
+#endif
+	layer_add_child(layer_window, text_layer_get_layer(layer_date));
+
+	text_layer_set_text(layer_date, "");
+	text_layer_set_background_color(layer_date, GColorBlack);
+	text_layer_set_text_color(layer_date, GColorWhite);
+	text_layer_set_text_alignment(layer_date, GTextAlignmentCenter);
+	text_layer_set_font(layer_date, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+
+#ifdef SHOW_SECONDS
+	tick_timer_service_subscribe(SECOND_UNIT, timer_tick);
+#else
+	tick_timer_service_subscribe(MINUTE_UNIT, timer_tick);
+#endif
 
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "bounds.size.w = %d", bounds.size.w);
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "col_spacing = %d", col_spacing);
@@ -106,8 +188,12 @@ static void window_load(Window *window) {
 
 static void window_unload(Window *window) {
 	tick_timer_service_unsubscribe();
-	text_layer_destroy(date_layer);
-	layer_destroy(main_layer);
+	text_layer_destroy(layer_date);
+#ifdef SHOW_SECONDS
+	layer_destroy(layer_sec);
+#endif
+	layer_destroy(layer_min);
+	layer_destroy(layer_hour);
 }
 
 static void init(void) {
